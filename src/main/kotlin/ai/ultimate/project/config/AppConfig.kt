e8b6ca97aaad61d6ae74125e5a26b2aa0ec0e4a0
Paras Diwan <prs.diwan@gmail.com>
2021-09-29 19:53:52 +0530

    add object mapper and http config

--- src/main/kotlin/ai/ultimate/project/config/AppConfig.kt
+++ src/main/kotlin/ai/ultimate/project/config/AppConfig.kt
@@ -1,10 +1,18 @@
 package ai.ultimate.project.config
 
+import ai.ultimate.project.config.AIClientConstants.AUTH_TOKEN
+import ai.ultimate.project.config.AIClientConstants.TIMEOUT_MINUTES
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.KotlinModule
 import okhttp3.OkHttpClient
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import java.time.Duration
 
 @Configuration
 @ComponentScan
@@ -13,6 +21,31 @@ class AppConfig {
 
     @Bean
     fun okHttpClient(): OkHttpClient {
-        return OkHttpClient()
+        val clientBuilder = OkHttpClient.Builder()
+        clientBuilder.addInterceptor { chain ->
+            val original = chain.request()
+
+            val request = original.newBuilder()
+                .addHeader("authorization", AUTH_TOKEN)
+                .addHeader(HttpHeaders.CONTENT_TYPE, APPLICATION_JSON_VALUE)
+                .build()
+
+            chain.proceed(request)
+        }
+        clientBuilder.connectTimeout(Duration.ofMinutes(TIMEOUT_MINUTES))
+        return clientBuilder.build()
     }
+
+    @Bean
+    fun objectMapper(): ObjectMapper {
+        val mapper = ObjectMapper()
+        mapper.registerModule(KotlinModule())
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+        return mapper
+    }
+}
+
+object AIClientConstants {
+    const val AUTH_TOKEN = "825765d4-7f8d-4d83-bb03-9d45ac9c27c0"
+    const val TIMEOUT_MINUTES: Long = 10
 }
