ffe107401591cba090f2235cf5784e834d69d11e
Paras Diwan <prs.diwan@gmail.com>
2021-09-29 19:56:03 +0530

    Cleanup and fix controller

--- src/main/kotlin/ai/ultimate/project/request/Controller.kt
+++ src/main/kotlin/ai/ultimate/project/request/Controller.kt
@@ -1,6 +1,11 @@
-package ai.ultimate.project
+package ai.ultimate.project.request
 
+import ai.ultimate.project.MessageProcessor
+import ai.ultimate.project.MessageReply
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
+import org.springframework.http.ResponseEntity
+import org.springframework.http.ResponseEntity.ok
 import org.springframework.web.bind.annotation.*
 
 @RestController
@@ -14,14 +19,15 @@ class Controller {
 
     @PostMapping(
         name = "/bots/{botId}/message",
-        consumes = ["application/json"],
-        produces = ["application/json"]
+        consumes = [APPLICATION_JSON_VALUE],
+        produces = [APPLICATION_JSON_VALUE]
     )
     fun processMessage(
         @PathVariable botId: String,
         @RequestBody body: Map<String, String>
-    ): String {
+    ): ResponseEntity<MessageReply> {
         val message = MessageDTO(botId, body.get("message"))
-        return messageProcessor.processReplyForMessage(message)
+        val replyForMessage = messageProcessor.processReplyForMessage(message)
+        return ok(replyForMessage)
     }
 }
