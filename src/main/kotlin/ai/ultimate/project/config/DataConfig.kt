ea4af97b8fbc6c207aaf3ef040522ea28b5518e2
Paras Diwan <prs.diwan@gmail.com>
2021-09-30 06:37:28 +0530

    Move data config to application properties

--- src/main/kotlin/ai/ultimate/project/config/DataConfig.kt
+++ src/main/kotlin/ai/ultimate/project/config/DataConfig.kt
@@ -4,6 +4,7 @@ import com.mongodb.ConnectionString
 import com.mongodb.MongoClientSettings
 import com.mongodb.client.MongoClient
 import com.mongodb.client.MongoClients
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration
@@ -14,16 +15,30 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 @EnableMongoRepositories(basePackages = ["ai.ultimate.project.data"])
 class DataConfig : AbstractMongoClientConfiguration() {
     private val DB_NAME = "ultimate"
+    @Value("\${mongodb.username}")
+    lateinit var dbUsername: String
+    @Value("\${mongodb.password}")
+    lateinit var dbPassword: String
+    @Value("\${mongodb.dbName}")
+    lateinit var dbName: String
+    @Value("\${mongodb.host}")
+    lateinit var dbHost: String
 
     override fun getDatabaseName(): String {
         return DB_NAME
     }
 
     override fun mongoClient(): MongoClient {
-        val connectionString = ConnectionString("mongodb://localhost:27017/ultimate")
-        val mongoClientSettings = MongoClientSettings.builder()
+        val connectionString =
+            ConnectionString("mongodb+srv://" +
+                    dbUsername + ":" +
+                    dbPassword + "@" +
+                    dbHost + "/" +
+                    dbName +
+                    "?retryWrites=true&w=majority")
+        val settings = MongoClientSettings.builder()
             .applyConnectionString(connectionString)
             .build()
-        return MongoClients.create(mongoClientSettings)
+        return MongoClients.create(settings)
     }
 }
