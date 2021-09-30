9365e97ac63aa02cc881bbe7b48d023c32bf5608
Paras Diwan <prs.diwan@gmail.com>
2021-09-30 06:38:53 +0530

    Add intent field

--- src/main/kotlin/ai/ultimate/project/data/ReplyByIntent.kt
+++ src/main/kotlin/ai/ultimate/project/data/ReplyByIntent.kt
@@ -1,11 +1,13 @@
 package ai.ultimate.project.data
 
+import org.bson.types.ObjectId
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 
-@Document
+@Document("repliesByIntent")
 data class ReplyByIntent(
     @Id
-    val id: String,
+    val id: ObjectId,
+    val intent: String,
     val reply: String
 )
